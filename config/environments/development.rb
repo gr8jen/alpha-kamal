587cfb76e9e583e3226f0deaa17e5fae14314cc5
Gert-Jan <g.lekkerkerker@mobie.nl>
2025-01-06 12:52:45 +0100

    config.host_authorization exclude up edit /up route

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,10 +76,5 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
 
-  # Skip http-to-https redirect for the default health check endpoint.
-  config.ssl_options = {
-    redirect: {
-      exclude: ->(request) { request.path == “/up” }
-    }
-  }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
 
-  # up is needed for successfull deploy with Kamal
-  get "up", to: ->(_env) { [200, { "Content-Type" => "text/plain" }, ["pong"]] }
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 end
